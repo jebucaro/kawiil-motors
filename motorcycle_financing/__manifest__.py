1419ee431b6c70ef181d6d83fa0c47bb94d7ec70
Jonathan Búcaro <hola@jonathanbucaro.com>
2025-07-23 08:08:45 -0600

    :bento: update manifest to add views

--- motorcycle_financing/__manifest__.py
+++ motorcycle_financing/__manifest__.py
@@ -11,6 +11,8 @@
 		'security/ir.model.access.csv',
 		'security/rules.xml',
 		'views/loan_application_views.xml',
+		'views/loan_application_tag_views.xml',
+		'views/loan_application_document_type_views.xml',
 		'views/motorcycle_financing_menu.xml',
 	],
 	'demo': [
